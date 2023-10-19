54ce418e21d7efef862551e1009acbbcb619a770
Arialdo Martini <arialdomartini@gmail.com>
2023-10-19 12:37:44 +0200

    Monadic apply

--- src/Monads.Test/Part4/IOSideEffects.cs
+++ src/Monads.Test/Part4/IOSideEffects.cs
@@ -4,13 +4,18 @@ using Xunit;
 
 namespace Monads.Test.Part4;
 
+record IO<B>(Func<B> f)
+{
+    internal B Run() => f();
+}
+
 public class IOSideEffects
 {
-    record IO<B>(Func<B> f)
+    public IOSideEffects()
     {
-        internal B Run() => f();
+        File.Delete("output.txt");
     }
-
+    
     [Fact]
     void running_the_IO_monad()
     {
@@ -33,4 +38,52 @@ public class IOSideEffects
         Assert.Equal(3, result);
         Assert.Equal("I'm a side effect!", File.ReadAllText("output.txt"));
     }
+    
+    /*
+Eff<int> Computation1() => ...
+Eff<int> Computation2() => ...
+Eff<int> Computation3() => ...
+
+Eff<int> result =
+    from value1 in Computation1()
+    from value2 in Computation2(value1)
+    from value3 in Computation3(value2, value1)
+    select value2;
+
+
+
+Option<int> Computation1() => ...
+Option<int> Computation2() => ...
+Option<int> Computation3() => ...
+
+Option<int> result =
+    from value1 in Computation1()
+    from value2 in Computation2(value1)
+    from value3 in Computation3(value2, value1)
+    select value2;
+
+
+Either<Error, int> Computation1() => ...
+Either<Error, int> Computation2() => ...
+Either<Error, int> Computation3() => ...
+
+Either<Error, int> result =
+    from value1 in Computation1()
+    from value2 in Computation2(value1)
+    from value3 in Computation3(value2, value1)
+    select value2;
+
+
+IEnumerable<int> Computation1() => ...
+IEnumerable<int> Computation2() => ...
+IEnumerable<int> Computation3() => ...
+
+IEnumerable<int> result =
+    from value1 in Computation1()
+    from value2 in Computation2(value1)
+    from value3 in Computation3(value2, value1)
+    select value2;
+
+*/
+    
 }
---
+++ src/Monads.Test/Part4/MonadicApply.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace Monads.Test.Part4;
+
+static class IOMonadicFunctionExtensions
+{
+    internal static IO<B> Apply<A, B>(this Func<A, IO<B>> f, IO<A> a) => 
+        new IO<B>(() =>
+        {
+            A aResult = a.Run();
+            IO<B> bResult = f(aResult);
+            return bResult.Run();
+        });
+}
+
+public class MonadicApply
+{
+    public MonadicApply()
+    {
+        File.Delete("output.txt");
+    }
+    
+    [Fact]
+    void native_csharp_apply()
+    {
+        // Apply :: (A -> B) -> A -> B
+        // Apply :: (A -> IO<B>) -> IO<A> -> IO<B>
+
+        // length :: string -> int
+        // double :: int -> double
+        Func<string, int> length = s => s.Length;
+        Func<int, decimal> @double = n => (decimal)n * 2;
+
+        var doubleTheLength = @double(length("foo"));
+
+        Assert.Equal(6, doubleTheLength);
+    }
+    
+    [Fact]
+    void monadic_apply()
+    {
+        // Apply :: (A -> B) -> A -> B
+        // Apply :: (A -> IO<B>) -> IO<A> -> IO<B>
+
+        // length :: string -> IO<int>
+        // double :: int -> IO<double>
+        Func<string, IO<int>> length = s =>
+            new IO<int>(() =>
+            {
+                File.WriteAllText("output.txt", "I'm a side effect!");
+                return s.Length;
+            });
+
+        Func<int, IO<double>> @double = n =>
+            new IO<double>(() =>
+            {
+                File.AppendAllText("output.txt", "I'm another side effect!");
+                return n * 2;
+            });
+
+        IO<int> monadicLength = length("foo");
+        IO<double> monadicResult = @double.Apply(monadicLength);
+
+        // Indeed, no file has been created yet
+        Assert.False(File.Exists("output.txt"));
+
+        var result = monadicResult.Run();
+
+        Assert.Equal(3*2, result);
+        Assert.Equal("I'm a side effect!I'm another side effect!", File.ReadAllText("output.txt"));
+
+    }
+}
